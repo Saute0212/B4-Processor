e6a97ae8d4d193802ff03165de8e2a815b02efec
Saute0212 <unknown@a.com>
2024-01-01 00:41:10 +0900

    20240101_0

--- src/main/scala/b4processor/modules/cache/InstructionMemoryCache.scala
+++ src/main/scala/b4processor/modules/cache/InstructionMemoryCache.scala
@@ -25,7 +25,7 @@ class InstructionMemoryCache(implicit params: Parameters) extends Module {
 
     val threadId = Input(UInt(log2Up(params.threads).W))
   })
-  
+
   io.fetch.request.ready := false.B
   io.fetch.response.bits := DontCare
   io.fetch.response.valid := false.B
@@ -41,6 +41,7 @@ class InstructionMemoryCache(implicit params: Parameters) extends Module {
 
   //アドレスを格納
   val addr = WireInit(UInt(64.W), 0.U)
+
   when(io.fetch.request.valid)
   {
     addr := io.fetch.request.bits
@@ -84,39 +85,46 @@ class InstructionMemoryCache(implicit params: Parameters) extends Module {
     AddrTagReg := AddrTag
   }
 
-  //ヒットしたかどうか判定
+  //ヒットするか判定
   val hitVec = WireInit(VecInit(Seq.fill(params.ICacheWay)(false.B)))
+  val hitVecReg = RegInit(VecInit(Seq.fill(params.ICacheWay)(false.B)))
   val hitWayNum = WireInit(0.U(log2Up(params.ICacheWay).W))
-  for(i <- 0 until params.ICacheWay)
+  when(io.fetch.request.valid)
   {
-    hitVec(i) := ICacheValidBit(i)(AddrIndex) && ICacheTag(i).read(AddrIndex) === AddrTag
-    when(hitVec(i))
+    for(i <- 0 until params.ICacheWay)
     {
-      hitWayNum := i.U
+      hitVec(i) := ICacheValidBit(i)(AddrIndex) && ICacheTag(i).read(AddrIndex) === AddrTag
+      hitVecReg(i) := hitVec(i)
+      when(hitVec(i))
+      {
+        hitWayNum := i.U
+      }
     }
   }
-
-  val hit = hitVec.reduce(_ || _)
 
   //BRAMからRead
   val ReadData = MuxLookup(hitWayNum, 0.U)((0 until params.ICacheWay).map(i => i.U -> ICacheDataBlock(i).read(AddrIndex)))
 
-  when(RegNext(hit, false.B))
+  val hit = hitVec.reduce(_ || _)
+
+  when(hit)
   {
     //ヒットした場合
-    val DataHitOut = MuxLookup(AddrOffsetReg, 0.U)(
+    val DataHitOut = MuxLookup(AddrOffset, 0.U)(
         (0 until params.ICacheDataNum).map(i => i.U -> ReadData((params.ICacheBlockWidth / params.ICacheDataNum) * (i + 1) - 1, (params.ICacheBlockWidth / params.ICacheDataNum) * i))
     )
     io.fetch.response.valid := true.B
     io.fetch.response.bits := DataHitOut
   } .otherwise {
     //ミスした場合
+    when(io.memory.response.valid)
+    {
+      io.memory.response.ready := true.B
+    }
 
-    io.memory.response.ready := true.B
-    
     //メモリからのデータ(64bit)をReadDataBufに格納
     when(io.memory.response.valid) {
-      ReadDataBuf(count) := io.memory.response.bits.value
+      ReadDataBuf(io.memory.response.bits.burstIndex) := io.memory.response.bits.value
       count := count + 1.U
     }
 
@@ -132,16 +140,15 @@ class InstructionMemoryCache(implicit params: Parameters) extends Module {
       io.fetch.response.valid := true.B
       io.fetch.response.bits := DataMissOut
 
-      count := 0.U
-
       //ウェイの選択を行い、キャッシュへ書き込む
+
       // SelectWay(AddrIndexReg) := SelectWay(AddrIndexReg) + 1.U
       // when(SelectWay(AddrIndexReg) === 0.U)
       // {
         //ウェイ0に書き込み
-        // ICacheDataBlock(0).write(AddrIndexReg, ReadDataCom)
-        // ICacheTag(0).write(AddrIndexReg, AddrTagReg)
-        // ICacheValidBit(0)(AddrIndexReg) := true.B
+        ICacheDataBlock(0).write(AddrIndexReg, ReadDataCom)
+        ICacheTag(0).write(AddrIndexReg, AddrTagReg)
+        ICacheValidBit(0)(AddrIndexReg) := true.B
         // count := 0.U
       // } .otherwise {
         //ウェイ1に書き込み
@@ -150,6 +157,10 @@ class InstructionMemoryCache(implicit params: Parameters) extends Module {
         // ICacheValidBit(1)(AddrIndexReg) := true.B
         // count := 0.U
       // }
+      when(io.fetch.response.ready)
+      {
+        count := 0.U
+      }
     }
   }
 }
@@ -184,6 +195,142 @@ h |  <------------  |  h  |  <------------  | r
   |   [Response]    |     |   [Response]    |
 */
 
+/*
+  io.fetch.request.ready := false.B
+  io.fetch.response.bits := DontCare
+  io.fetch.response.valid := false.B
+
+  io.memory.request.valid := false.B
+  io.memory.request.bits := DontCare
+  io.memory.response.ready := false.B
+
+  io.memory.request.bits.size := MemoryAccessWidth.DoubleWord
+  io.memory.request.bits.burstLength := (params.MemoryBurstLength-1).U
+
+  io.memory.request.valid := io.fetch.request.valid
+  // io.fetch.request.ready := io.memory.request.ready
+
+  //アドレスを格納
+  val addr = WireInit(UInt(64.W), 0.U)
+  when(io.fetch.request.valid){
+    addr := io.fetch.request.bits
+    io.memory.request.bits.address := io.fetch.request.bits(63, 6) ## 0.U(6.W)
+    when(io.memory.request.ready)
+    {
+      io.fetch.request.ready := true.B
+    }
+  }
+
+  //タグ・インデックス・オフセットの抽出
+  val IgnoreBits = log2Up(16)
+  val OffsetBits = log2Up(params.ICacheDataNum)
+  val IndexBits = log2Up(params.ICacheSet)
+  val TagBits = 64 - IndexBits - OffsetBits - IgnoreBits
+  val AddrOffset = addr(IgnoreBits + OffsetBits - 1, IgnoreBits)
+  val AddrOffsetReg = RegInit(0.U(OffsetBits.W))
+  val AddrIndex = addr(IgnoreBits + OffsetBits + IndexBits - 1, IgnoreBits + OffsetBits)
+  val AddrIndexReg = RegInit(0.U(IndexBits.W))
+  val AddrTag = addr(63, IgnoreBits + OffsetBits + IndexBits)
+  val AddrTagReg = RegInit(0.U(TagBits.W))
+
+  //有効ビット・タグ・インデックス
+  val ICacheValidBit = RegInit(VecInit(Seq.fill(params.ICacheWay)(VecInit(Seq.fill(params.ICacheSet)(false.B)))))
+  val ICacheTag = Seq.fill(params.ICacheWay)(SyncReadMem(params.ICacheSet, UInt(TagBits.W)))
+  val ICacheDataBlock = Seq.fill(params.ICacheWay)(SyncReadMem(params.ICacheSet, UInt(params.ICacheBlockWidth.W)))
+
+  //バースト転送のバッファー
+  val ReadDataBuf = RegInit(VecInit(Seq.fill(params.MemoryBurstLength)(0.U(64.W))))
+
+  //バースト転送のカウンター
+  val count = RegInit(0.U(8.W))
+
+  //ウェイのカウンター(各セットごとにカウンターを用意)
+  val SelectWay = RegInit(VecInit(Seq.fill(params.ICacheSet)(0.U(1.W))))
+
+  //タグ・インデックス・オフセットをレジスタに格納
+  when(io.fetch.request.valid) {
+    AddrOffsetReg := AddrOffset
+    AddrIndexReg := AddrIndex
+    AddrTagReg := AddrTag
+  }
+
+  //ヒットしたかどうか判定
+  val hitVec = RegInit(VecInit(Seq.fill(params.ICacheWay)(false.B)))
+  val hitWayNum = RegInit(0.U(log2Up(params.ICacheWay).W))
+  for(i <- 0 until params.ICacheWay)
+  {
+    hitVec(i) := ICacheValidBit(i)(AddrIndex) && ICacheTag(i).read(AddrIndex) === AddrTag
+    when(hitVec(i))
+    {
+      hitWayNum := i.U
+    }
+  }
+
+  val hit = hitVec.reduce(_ || _)
+
+  //BRAMからRead
+  val ReadData = MuxLookup(hitWayNum, 0.U)((0 until params.ICacheWay).map(i => i.U -> ICacheDataBlock(i).read(AddrIndex)))
+
+  when(RegNext(hit, false.B))
+  {
+    //ヒットした場合
+    val DataHitOut = MuxLookup(AddrOffsetReg, 0.U)(
+        (0 until params.ICacheDataNum).map(i => i.U -> ReadData((params.ICacheBlockWidth / params.ICacheDataNum) * (i + 1) - 1, (params.ICacheBlockWidth / params.ICacheDataNum) * i))
+    )
+    io.fetch.response.valid := true.B
+    io.fetch.response.bits := DataHitOut
+  } .otherwise {
+    //ミスした場合
+  }
+*/
+/*
+    //ミスした場合
+    when(io.memory.response.valid)
+    {
+      io.memory.response.ready := true.B
+    }
+
+    //メモリからのデータ(64bit)をReadDataBufに格納
+    when(io.memory.response.valid) {
+      ReadDataBuf(io.memory.response.bits.burstIndex) := io.memory.response.bits.value
+      count := count + 1.U
+    }
+
+    when(count === 8.U)
+    {
+      io.memory.response.ready := false.B
+
+      //データをリトルエンディアンでReadDataComへ格納
+      val ReadDataCom = Cat(ReadDataBuf.reverse)
+      val DataMissOut = MuxLookup(AddrOffsetReg, 0.U)(
+          (0 until params.ICacheDataNum).map(i => i.U -> ReadDataCom((params.ICacheBlockWidth / params.ICacheDataNum) * (i + 1) - 1, (params.ICacheBlockWidth / params.ICacheDataNum) * i))
+      )
+      io.fetch.response.valid := true.B
+      io.fetch.response.bits := DataMissOut
+
+      //ウェイの選択を行い、キャッシュへ書き込む
+
+      // SelectWay(AddrIndexReg) := SelectWay(AddrIndexReg) + 1.U
+      // when(SelectWay(AddrIndexReg) === 0.U)
+      // {
+        //ウェイ0に書き込み
+        // ICacheDataBlock(0).write(AddrIndexReg, ReadDataCom)
+        // ICacheTag(0).write(AddrIndexReg, AddrTagReg)
+        // ICacheValidBit(0)(AddrIndexReg) := true.B
+        // count := 0.U
+      // } .otherwise {
+        //ウェイ1に書き込み
+        // ICacheDataBlock(1).write(AddrIndexReg, ReadDataCom)
+        // ICacheTag(1).write(AddrIndexReg, AddrTagReg)
+        // ICacheValidBit(1)(AddrIndexReg) := true.B
+        // count := 0.U
+      // }
+      when(io.fetch.response.ready)
+      {
+        count := 0.U
+      }
+    }
+*/
 /*
   io.fetch.request.ready := false.B
   io.fetch.response.bits := DontCare
