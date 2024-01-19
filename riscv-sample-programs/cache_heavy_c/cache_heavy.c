5ad0afe874a440e6ea8f74f4b80ffa0619aecd5b
Saute0212 <unknown@a.com>
2024-01-19 15:27:30 +0900

    20240119_1

---
+++ riscv-sample-programs/cache_heavy_c/cache_heavy.c
@@ -0,0 +1,56 @@
+#define N 100
+
+int div(int a, int b)
+{
+    int tmp = 0;
+    tmp = a/b;
+    return tmp;
+}
+
+int mul(int a, int b)
+{
+    int tmp = 0;
+    tmp = a*b;
+    return tmp;
+}
+
+int main()
+{
+    //"input1" < "input2"
+    int input1[N][N] = {0};
+    int input2[N][N] = {0};
+    int tmp1[N][N] = {0};
+    int tmp2[N][N] = {0};
+    int output[N][N] = {0};
+
+    for(int run_count = 0; run_count < 3; run_count++)
+    {
+        for(int i = 0; i < N; i++)
+        {
+            for(int j = 0; j < N; j++)
+            {
+                input1[i][j] = i+1;
+                input2[i][j] = i+j+2;
+            }
+        }
+
+        for(int i = 0; i < N; i++)
+        {
+            for(int j = 0; j < N; j++)
+            {
+                tmp1[i][j] = div(input1[i][j], input2[i][j]);
+                tmp2[i][j] = mul(input1[i][j], input2[i][j]);
+            }
+        }
+
+        for(int i = 0; i < N; i++)
+        {
+            for(int j = 0; j < N; j++)
+            {
+                output[i][j] = tmp1[i][j]+tmp2[i][j];
+            }
+        }
+    }
+
+    return output[5][7];
+}
