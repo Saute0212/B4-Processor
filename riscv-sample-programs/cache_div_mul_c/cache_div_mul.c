bc16a5fb549c61ce8b97a8b1880a4a4a334659be
Saute0212 <unknown@a.com>
2024-01-23 17:28:21 +0900

    20240123_2

--- riscv-sample-programs/cache_div_mul_c/cache_div_mul.c
+++ riscv-sample-programs/cache_div_mul_c/cache_div_mul.c
@@ -1,5 +1,31 @@
+//a/b b!=0
+int div(int a, int b)
+{
+    int tmp, answer;
+
+    return answer;
+}
+
+//a*b
+int mul(int a, int b)
+{
+    int answer;
+    if(a==0 || b==0)
+    {
+        answer = 0;
+    } else {
+        for(int i=0; i<b; i++)
+        {
+            answer += a;
+        }
+    }
+    return answer;
+}
+
 int main()
 {
+    int num;
+    num=div(5,7);
     __asm__ volatile("li x26,7");
-    return 0;
+    return mul;
 }
