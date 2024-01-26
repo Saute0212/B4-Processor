f96b468531317ecedeac41c74715e5556ac2f0e4
Saute0212 <unknown@a.com>
2024-01-27 00:52:01 +0900

    20240127_0

--- riscv-sample-programs/cache_heavy_c/cache_heavy.c
+++ riscv-sample-programs/cache_heavy_c/cache_heavy.c
@@ -1,10 +1,11 @@
-#define NOP8() __asm__ volatile("nop;nop;nop;nop;nop;nop;nop;nop;")
-#define NOP32() do {NOP8(); NOP8(); NOP8(); NOP8(); } while(0)
+#define NOP8() __asm__ volatile("nop;nop;nop;nop;nop;nop;nop;nop;") //128bit
+#define NOP32() do {NOP8(); NOP8(); NOP8(); NOP8(); } while(0) //512bit
 
 int main()
 {
     for(int i=0; i < 10; i++)
     {
+        //512bit*10
         NOP32();
         NOP32();   
         NOP32();
